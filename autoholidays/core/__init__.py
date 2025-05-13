9f4dde23a33cd5a93d116425a4cf6d700dcc80a1
ZenithClown <debmalyapramanik.005@gmail.com>
2025-05-13 10:35:47 +0530

    ✨ register autoholidays/core, added pydantic in requirments.txt

---
+++ autoholidays/core/__init__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+
+"""
+Core Module to Create Constructs for the Holiday Planner
+
+The submodule provides essential functions and classes constructs for
+calcuating optimal leave planning based on end user requirements. A
+native base construct uses :mod:`pydantic` class for data validation
+and serialization.
+"""
+
