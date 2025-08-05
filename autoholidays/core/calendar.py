81ec5e572b1dc4ea79ff4bcc7d63721629483aa6
ZenithClown <debmalyapramanik.005@gmail.com>
2025-08-05 23:37:43 +0530

    ✨ create attributes to return holidays for each person, update docstring

--- autoholidays/core/calendar.py
+++ autoholidays/core/calendar.py
@@ -8,9 +8,10 @@ holidays - paid, regional, etc.
 
 import datetime as dt
 
-from typing import List
+from typing import List, Dict
 from pydantic import BaseModel, model_validator
 
+from autoholidays.core.static import ENUMDays
 from autoholidays.core.person import PersonConstruct
 
 
@@ -36,6 +37,25 @@ class PlanningCycle(BaseModel):
     persons : List[PersonConstruct]
 
 
+    @property
+    def personHolidays(self) -> Dict[int, List[dt.date]]:
+        """
+        Returns a list of leaves based on regional holidays (planned
+        leaves) and week-offs as per the planning cycle. The dates
+        are always unique and sorted in ascending order.
+        """
+
+        return {
+            idx : sorted([
+                day for day in person.holidays + self.weekoffs(
+                    self.allDates, person.weekoff
+                )
+                if day >= self.start and day <= self.final
+            ])
+            for idx, person in enumerate(self.persons)
+        }
+
+
     @model_validator(mode = "after")
     def checkDates(self) -> 'PlanningCycle':
         assert self.start <= self.final, \
@@ -46,7 +66,39 @@ class PlanningCycle(BaseModel):
 
     @property
     def allDates(self) -> List[dt.date]:
+        """
+        Returns a list of all calendar dates between the start and
+        the final date, i.e., the planning cycle.
+        """
+
         return [
             self.start + dt.timedelta(days = days)
             for days in range((self.final - self.start).days + 1)
+        ]
+
+
+    @staticmethod
+    def weekoffs(
+        dates : List[dt.date], weekoffs : List[ENUMDays]
+    ) -> List[dt.date]:
+        """
+        Returns a list of date in the current planning cycle where
+        a person is entitled for a weekly off. This is a static
+        function, thus allowing to calculate for each person on the
+        fly. The function does not consider the planning cycle.
+
+        :type  dates: List[dt.date]
+        :param dates: A list of dates all dates in the planning cycle,
+            typically this is always ``self.allDates`` property.
+
+        :type  weekoffs: List[ENUMDays]
+        :param weekoffs: A list of enumerated weekoffs, of type
+            :class:`ENUMDays` that is used to calculate valid dates in
+            the planning cycle.
+        """
+
+        return [
+            date for date in dates if date.weekday() in [
+                wk.value for wk in weekoffs
+            ]
         ]
