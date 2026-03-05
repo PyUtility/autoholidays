1f69da873853459107aa34230aa173bbce34985e
E33605 <debmalyapramanik.005@gmail.com>
2026-03-05 11:39:36 +0530

    💣 added version control in autoholidays/__init__.py
    - also removed dependency check from code file, should be handled during installation

--- autoholidays/__init__.py
+++ autoholidays/__init__.py
@@ -12,27 +12,9 @@ individuals and teams, it supports coordinated planning and adapts to
 diverse organizational policies and regional calendars.
 """
 
-import os
+__version__ = "v0.0.2.dev0"
 
-# ? package follows https://peps.python.org/pep-0440/
-with open(os.path.join(os.path.dirname(__file__), "VERSION")) as fh:
-    __version__ = fh.read().strip()
-
-# ! let's check for package hard dependencies which must be available
-hard_dependencies = ["pydantic"]
-missing_dependencies = []
-
-for dependency in hard_dependencies:
-    try:
-        __import__(dependency)
-    except ImportError as err:
-        missing_dependencies.append(err.name)
-
-if missing_dependencies:
-    raise ImportError(
-        f"Missing hard dependencies: {missing_dependencies}."
-    )
-
+# ? added init time options registrations from autoholidays.api
 from autoholidays.api import *  # noqa: F403, E402
 
 __all__ = [
