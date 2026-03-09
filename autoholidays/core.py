0ef769b9824c9ff264293cabece9635133e5114a
ZenithClown <debmalyapramanik.005@gmail.com>
2026-03-09 22:18:38 +0530

    ✨ add methods to validate parameters for planning, added static method
    - added static method to return a list of valid weekends for the planning year
    - added __validate_strategy__() to validate strategy values
    - added __validate_holidays__() to update and check valid holidays

--- autoholidays/core.py
+++ autoholidays/core.py
@@ -6,14 +6,46 @@ and finally strategizes an optimal holiday for a person or a group of
 person based on different scenarios.
 """
 
+import warnings
 import datetime as dt
 
 from typing import List
-from autoholidays.calendar_ import PlanningCycle
+
+from autoholidays.person import PersonConstruct
+from autoholidays.calendar import PlanningCycle, ENUMDays
 
 class AutoHoliday:
-    def __init__(self, planning : PlanningCycle) -> None:
+    """
+    A class to plan an optimal holiday for a person or a group of
+    person. The class is initialized with a planning cycle, set of
+    person(s), and a strategy to plan the holiday.
+
+    :type  persons: List[PersonConstruct]
+    :param persons: A list of person(s) to plan for, uses the persons
+        construct defined in :mod:`autoholidays.person`. At least,
+        one person is required for planning.
+
+    :type  planning: PlanningCycle
+    :param planning: A fixed planning cycle which is same across all
+        the person(s) to plan holidays.
+
+    :type  strategy: str
+    :param strategy: A planning strategy that dustributes the holiday
+        in a specific way. Defaults to ``balanced`` approach - that
+        mixes short and long holidays.
+    """
+
+    def __init__(
+        self,
+        persons : List[PersonConstruct],
+        planning : PlanningCycle,
+        strategy : str = "balanced"
+    ) -> None:
         self.planning = planning
+
+        self.persons = self.__validate_holidays__(persons)
+        self.strategy = self.__validate_strategy__(strategy)
+        return persons
 
 
     @staticmethod
@@ -60,3 +92,73 @@ class AutoHoliday:
                 dt.datetime.fromordinal(date).date() for date in group
             ] for group in groups
         ]
+
+
+    @staticmethod
+    def weekOffs(
+        dates : List[dt.date], weekends : List[ENUMDays]
+    ) -> List[dt.date]:
+        """
+        Returns a list of date in the current planning cycle where
+        a person is entitled for a weekly off. This is a static
+        function, thus allowing to calculate for each person on the
+        fly. The function does not consider the planning cycle.
+
+        :type  dates: List[dt.date]
+        :param dates: A list of all dates in the planning cycle,
+            typically this is always ``self.allDates`` property.
+
+        :type  weekoffs: List[ENUMDays]
+        :param weekoffs: A list of enumerated weekoffs, of type
+            :class:`ENUMDays` that is used to calculate valid dates in
+            the planning cycle.
+        """
+
+        return [
+            date for date in dates if date.weekday() in [
+                wk.value for wk in weekends
+            ]
+        ]
+
+
+    def __validate_strategy__(self, strategy : str) -> str:
+        """
+        Validate the strategy value based on the list of valid
+        strategies. The function raises an assertion error if the
+        strategy is invalid. The following types of strategies are
+        available:
+
+            * balanced: A strategy that mixes short and long holidays
+                to provide a balanced holiday plan for the person(s).
+        """
+
+        valid = ["balanced"]
+        assert strategy in valid, f"Invalid Strategy: {strategy}"
+
+        return strategy
+
+
+    def __validate_holidays__(
+        self,
+        persons : List[PersonConstruct]
+    ) -> List[PersonConstruct]:
+        """
+        Validate the list of holidays for the person(s) and returns
+        valid days, this is helpful in case of invalid holidays, i.e.,
+        any holiday that is falling outside of the planning cycle.
+        """
+
+        start, final = self.planning.start, self.planning.final
+        for person in persons:
+            current = person.holidays
+            updated = [
+                holiday for holiday in person.holidays
+                if holiday >= start and holiday <= final
+            ]
+
+            if current != updated:
+                warnings.warn(f"Holiday's Updated for {person.name}")
+                person.holidays = sorted(updated)
+            else:
+                person.holidays = sorted(current)
+
