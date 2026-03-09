b40b799406803b9a15edeac0f485349d9a1bd74d
ZenithClown <debmalyapramanik.005@gmail.com>
2026-03-09 21:48:48 +0530

    💣 refactor autoholidays.calendar.PlannignCycle
    - added basic requirements for the planning cycle, let other things be handled by the planner

--- autoholidays/calendar.py
+++ autoholidays/calendar.py
@@ -8,8 +8,9 @@ are defined which is used as an utility functions to plan leaves.
 import datetime as dt
 
 from enum import Enum
-from pydantic import BaseModel, Field
+from typing import List
 
+from pydantic import BaseModel, Field, model_validator
 
 class ENUMDays(Enum):
     """
@@ -49,3 +50,46 @@ class CreditDays(BaseModel):
     name : str = Field(..., description = "Name/Type of Leave")
     days : int = Field(..., gt = 0, description = "Number of Days")
     date : dt.date = Field(..., description = "Date of Leave Credit")
+
+
+class PlanningCycle(BaseModel):
+    """
+    A default construct to represent a planning cycle. The planning
+    cycle construct defines a start and final date and is fixed for
+    all the members during a optimal holiday run. The construct is
+    defined such that any length of period can be provided to find the
+    best optimal holiday for the user(s).
+
+    :type  start, final: dt.date
+    :param start, final: First and last date (inclusive) for the
+        planning cycle.
+    """
+
+    start : dt.date
+    final : dt.date
+
+
+    @model_validator(mode = "after")
+    def validateCycle(self) -> "PlanningCycle":
+        """
+        Validate the planning cycle and raise an error if the final
+        date is before the start date.
+        """
+
+        assert self.start <= self.final, \
+            "Start Date ≥ Final Date is Invalid"
+
+        return self
+
+
+    @property
+    def allDays(self) -> List[dt.date]:
+        """
+        Returns a list of all calendar dates between the start and
+        the final date, i.e., the planning cycle.
+        """
+
+        return [
+            self.start + dt.timedelta(days = days)
+            for days in range((self.final - self.start).days + 1)
+        ]
